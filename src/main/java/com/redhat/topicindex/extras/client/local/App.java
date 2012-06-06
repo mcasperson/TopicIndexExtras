5735910ab8287cdbede367776e069d789483423b
Matthew Casperson <mcaspers@redhat.com>
2012-06-06 12:37:53 +1000

    Uploads now working

--- src/main/java/com/redhat/topicindex/extras/client/local/App.java
+++ src/main/java/com/redhat/topicindex/extras/client/local/App.java
@@ -10,20 +10,23 @@ import org.jboss.errai.bus.client.api.RemoteCallback;
 import org.jboss.errai.enterprise.client.jaxrs.api.RestClient;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.vectomatic.file.File;
-import org.vectomatic.file.FileList;
 import org.vectomatic.file.FileReader;
-import org.vectomatic.file.FileUploadExt;
 import org.vectomatic.file.events.LoadEndEvent;
 import org.vectomatic.file.events.LoadEndHandler;
 
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.regexp.shared.MatchResult;
 import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.redhat.topicindex.rest.entities.interfaces.RESTImageV1;
 
@@ -35,9 +38,14 @@ public class App
 	private static final String RESTIMAGEV1_ID_RE = "\"id\":(\\d+)";
 	private static final RegExp RESTIMAGEV1_ID_EXP = RegExp.compile(RESTIMAGEV1_ID_RE);
 	private static final String REST_SERVER = "http://localhost:8080/TopicIndex/seam/resource/rest";
+
 	// private static final String REST_SERVER = "http://skynet-dev.usersys.redhat.com:8080/TopicIndex/seam/resource/rest";
 
-	private TextArea resultsTextArea;
+	private final List<ImageUploadData> imageUploadBlocks = new ArrayList<ImageUploadData>();
+	private final TextBox prefix = new TextBox();
+	private final TextArea finalResults = new TextArea(); 
+	final Button upload = new Button("Upload");
+	final Button newLangButton = new Button("New Language");
 
 	@PostConstruct
 	public void init()
@@ -45,120 +53,188 @@ public class App
 		/* Init the REST service */
 		RestClient.setApplicationRoot(REST_SERVER);
 		RestClient.setJacksonMarshallingActive(true);
+		
+		finalResults.setReadOnly(true);
+		finalResults.setWidth("500px");
+		finalResults.setHeight("300px");
+		finalResults.setVisible(false);
+
+		final Grid layoutGrid = new Grid(4, 1);
+
+		final HorizontalPanel prefixLayout = new HorizontalPanel();
+		layoutGrid.setWidget(0, 0, prefixLayout);
+
+		final Label prefixLabel = new Label("Enter an optional image name prefix");
+		prefixLabel.getElement().getStyle().setPaddingRight(10, Unit.PX);
+		prefixLayout.add(prefixLabel);
+		prefixLayout.add(prefix);
 
 		final VerticalPanel verticalPanel = new VerticalPanel();
+		layoutGrid.setWidget(1, 0, verticalPanel);
 
-		final Label loadImagesLabel = new Label("Please select the images you wish to upload");
-		verticalPanel.add(loadImagesLabel);
+		final ImageUploadData initialBlock = new ImageUploadData();
+		imageUploadBlocks.add(initialBlock);
+		verticalPanel.add(initialBlock.getGrid());
+		
+		layoutGrid.setWidget(2, 0, finalResults);
 
-		final FileUploadExt upload = new FileUploadExt();
-		verticalPanel.add(upload);
+		final HorizontalPanel horizontalLayout = new HorizontalPanel();
+		layoutGrid.setWidget(3, 0, horizontalLayout);
 
-		upload.addChangeHandler(new ChangeHandler()
+		
+		newLangButton.addClickHandler(new ClickHandler()
 		{
 			@Override
-			public void onChange(final ChangeEvent event)
+			public void onClick(final ClickEvent event)
 			{
-				final FileList files = upload.getFiles();
-				final StringBuilder text = new StringBuilder();
-				for (int i = 0; i < files.getLength(); ++i)
-				{
-					processImage(files.getItem(i), text, i == files.getLength() - 1);
-				}
+				final ImageUploadData newBlock = new ImageUploadData();
+				imageUploadBlocks.add(newBlock);
+				verticalPanel.add(newBlock.getGrid());
+			}
+		});
+		horizontalLayout.add(newLangButton);
+
+		
+		horizontalLayout.add(upload);
+
+		upload.addClickHandler(new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent event)
+			{
+
+				setEnabled(false);	
+				
+				finalResults.setText("");
+				finalResults.setVisible(false);
+				
+				final StringBuilder results = new StringBuilder();
+				processImage(0, 0, results);
 			}
 		});
 
-		final Label resultsLabel = new Label("The results below show the original file names, and the new CCMS file names");
-		verticalPanel.add(resultsLabel);
-
-		resultsTextArea = new TextArea();
-		resultsTextArea.setReadOnly(true);
-		verticalPanel.add(resultsTextArea);
-
-		RootPanel.get().add(verticalPanel);
+		RootPanel.get().add(layoutGrid);
+	}
+	
+	private void uploadDone(final StringBuilder results)
+	{
+		finalResults.setText(results.toString());
+		finalResults.setVisible(true);
+		setEnabled(true);
 	}
 
-	private void processImage(final File file, final StringBuilder results, final boolean lastFile)
+	private void processImage(final int blockIndex, final int fileIndex, final StringBuilder results)
 	{
-		final RemoteCallback<String> genericSuccessCallback = new RemoteCallback<String>()
+		/* there are no more blocks to process, so enable the UI and return */
+		if (blockIndex >= this.imageUploadBlocks.size())
+		{					
+			uploadDone(results);			
+			return;
+		}
+
+		/* see if there are any more files to process */
+		final ImageUploadData data = imageUploadBlocks.get(blockIndex);
+
+		/* There are no more files to process, so move to the next block */
+		if (fileIndex >= data.getUpload().getFiles().getLength())
 		{
-			@Override
-			public void callback(final String retValue)
+			processImage(blockIndex + 1, 0, results);
+		}
+		/* There are files to process, process them and then move to the next file */
+		else
+		{
+
+			final File file = data.getUpload().getFiles().getItem(fileIndex);
+
+			final RemoteCallback<String> genericSuccessCallback = new RemoteCallback<String>()
 			{
-				/* temp workaround for bug at https://community.jboss.org/thread/200710?tstart=0 */				
-				final MatchResult filenameMatcher = RESTIMAGEV1_FILENAME_EXP.exec(retValue);
-				final boolean filenameMatchFound = RESTIMAGEV1_FILENAME_EXP.test(retValue);
-				
-				final MatchResult idMatcher = RESTIMAGEV1_ID_EXP.exec(retValue);
-				final boolean idMatchFound = RESTIMAGEV1_ID_EXP.test(retValue);
-				
-				if (filenameMatchFound && idMatchFound)
-				{				
-					final String filename = filenameMatcher.getGroup(1);
-					final String id = idMatcher.getGroup(1);
+				@Override
+				public void callback(final String retValue)
+				{
+					/* temp workaround for bug at https://community.jboss.org/thread/200710?tstart=0 */
+					final MatchResult filenameMatcher = RESTIMAGEV1_FILENAME_EXP.exec(retValue);
+					final boolean filenameMatchFound = RESTIMAGEV1_FILENAME_EXP.test(retValue);
+
+					final MatchResult idMatcher = RESTIMAGEV1_ID_EXP.exec(retValue);
+					final boolean idMatchFound = RESTIMAGEV1_ID_EXP.test(retValue);
+
+					if (filenameMatchFound && idMatchFound)
+					{
+						final String filename = prefix.getText() + filenameMatcher.getGroup(1);
+						final String id = idMatcher.getGroup(1);
+
+						results.append(id + ": " + filename + "\n");
+					}
+
+					processImage(blockIndex, fileIndex + 1, results);
+				}
+			};
+
+			final RemoteCallback<RESTImageV1> successCallback = new RemoteCallback<RESTImageV1>()
+			{
+				@Override
+				public void callback(final RESTImageV1 retValue)
+				{
+					results.append(retValue.getFilename() + ": " + retValue.getId().toString() + "\n");
 					
-					results.append(id + ": " + filename + "\n");
-					if (lastFile)
-						resultsTextArea.setText(results.toString());
+					processImage(blockIndex, fileIndex + 1, results);
 				}
-			}
-		};
+			};
 
-		final RemoteCallback<RESTImageV1> successCallback = new RemoteCallback<RESTImageV1>()
-		{
-			@Override
-			public void callback(final RESTImageV1 retValue)
+			final ErrorCallback errorCallback = new ErrorCallback()
 			{
-				results.append(retValue.getFilename() + ": " + retValue.getId().toString() + "\n");
-				if (lastFile)
-					resultsTextArea.setText(results.toString());
-			}
-		};
-
-		final ErrorCallback errorCallback = new ErrorCallback()
-		{
-			@Override
-			public boolean error(Message message, Throwable throwable)
-			{
-				results.append(file.getName() + ": ERROR!");
-				if (lastFile)
-					resultsTextArea.setText(results.toString());
-				return true;
-			}
-		};
-
-		final RESTInterfaceV1 restMethod = RestClient.create(RESTInterfaceV1.class, genericSuccessCallback, errorCallback);
-
-		final FileReader reader = new FileReader();
-		reader.addLoadEndHandler(new LoadEndHandler()
-		{
-			@Override
-			public void onLoadEnd(LoadEndEvent event)
-			{
-
-				final String result = reader.getStringResult();
-				final byte[] buffer = getByteArray(result, 1);
-
-				final RESTImageV1 image = new RESTImageV1();
-				image.explicitSetImageData(buffer);
-				image.explicitSetFilename(file.getName());
-
-				try
+				@Override
+				public boolean error(Message message, Throwable throwable)
 				{
-					restMethod.createJSONImage("", image);
+					results.append(file.getName() + ": ERROR!");
+					processImage(blockIndex, fileIndex + 1, results);					
+					return true;
 				}
-				catch (final Exception ex)
+			};
+
+			final RESTInterfaceV1 restMethod = RestClient.create(RESTInterfaceV1.class, genericSuccessCallback, errorCallback);
+
+			final FileReader reader = new FileReader();
+			reader.addLoadEndHandler(new LoadEndHandler()
+			{
+				@Override
+				public void onLoadEnd(LoadEndEvent event)
 				{
-					ex.printStackTrace();
-					Window.alert(ex.toString());
 
-					if (lastFile)
-						resultsTextArea.setText(results.toString());
+					final String result = reader.getStringResult();
+					final byte[] buffer = getByteArray(result, 1);
+
+					final RESTImageV1 image = new RESTImageV1();
+					image.explicitSetImageData(buffer);
+					image.explicitSetFilename(file.getName());
+
+					try
+					{
+						restMethod.createJSONImage("", image);
+					}
+					catch (final Exception ex)
+					{
+						ex.printStackTrace();
+						Window.alert(ex.toString());
+					}
 				}
-			}
-		});
+			});
 
-		reader.readAsBinaryString(file);
+			reader.readAsBinaryString(file);
+		}
+	}
+	
+	private void setEnabled(final boolean enabled)
+	{
+		for (final ImageUploadData block : imageUploadBlocks)
+		{
+			block.setEnabled(enabled);
+		}
+		
+		this.finalResults.setEnabled(enabled);
+		this.prefix.setEnabled(enabled);
+		this.upload.setEnabled(enabled);
+		this.newLangButton.setEnabled(enabled);
 	}
 
 	public static byte[] getByteArray(final String string, final int bytesPerChar)
---
+++ src/main/java/com/redhat/topicindex/extras/client/local/ImageUploadData.java
@@ -0,0 +1,82 @@
+package com.redhat.topicindex.extras.client.local;
+
+import org.vectomatic.file.File;
+import org.vectomatic.file.FileList;
+import org.vectomatic.file.FileUploadExt;
+
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextArea;
+
+public class ImageUploadData
+{
+	private static final String[] LOCALES = {"ar", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB", "ar-LY", "ar-MA", "ar-OM", "ar-QA", "ar-SA", "ar-SD", "ar-SY", "ar-TN", "ar-YE", "be", "be-BY", "bg", "bg-BG", "ca", "ca-ES", "cs", "cs-CZ", "da", "da-DK", "de", "de-AT", "de-CH", "de-DE", "de-LU", "el", "el-CY", "el-GR", "en", "en-AU", "en-CA", "en-GB", "en-IE", "en-IN", "en-MT", "en-NZ", "en-PH", "en-SG", "en-US", "en-ZA", "es", "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-DO", "es-EC", "es-ES", "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PE", "es-PR", "es-PY", "es-SV", "es-US", "es-UY", "es-VE", "et", "et-EE", "fi", "fi-FI", "fr", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "fr-LU", "ga", "ga-IE", "hi-IN", "hr", "hr-HR", "hu", "hu-HU", "in", "in-ID", "is", "is-IS", "it", "it-CH", "it-IT", "iw", "iw-IL", "ja", "ja-JP", "ja-JP-JP-#u-ca-japanese", "ko", "ko-KR", "lt", "lt-LT", "lv", "lv-LV", "mk", "mk-MK", "ms", "ms-MY", "mt", "mt-MT", "nl", "nl-BE", "nl-NL", "no", "no-NO", "no-NO-NY", "pl", "pl-PL", "pt", "pt-BR", "pt-PT", "ro", "ro-RO", "ru", "ru-RU", "sk", "sk-SK", "sl", "sl-SI", "sq", "sq-AL", "sr", "sr-BA", "sr-BA-#Latn", "sr-CS", "sr-ME", "sr-ME-#Latn", "sr-RS", "sr-RS-#Latn", "sr--#Latn", "sv", "sv-SE", "th", "th-TH", "th-TH-TH-#u-nu-thai", "tr", "tr-TR", "uk", "uk-UA", "vi", "vi-VN", "zh", "zh-CN", "zh-HK", "zh-SG", "zh-TW"};
+	private final FileUploadExt upload = new FileUploadExt();
+	private final TextArea fileList = new TextArea();
+	private final ListBox language = new ListBox(false);
+	private final Grid grid = new Grid(3, 2);
+
+	public FileUploadExt getUpload()
+	{
+		return upload;
+	}
+
+	public TextArea getFileList()
+	{
+		return fileList;
+	}
+
+	public ListBox getLanguage()
+	{
+		return language;
+	}
+	
+	public Grid getGrid()
+	{
+		return grid;
+	}
+	
+	public ImageUploadData()
+	{
+		fileList.setReadOnly(true);
+		fileList.setWidth("500px");
+		fileList.setHeight("200px");
+		
+		for (final String locale : LOCALES)
+			language.addItem(locale);
+		
+		upload.addChangeHandler(new ChangeHandler()
+		{
+			@Override
+			public void onChange(final ChangeEvent event)
+			{
+				final FileList files = upload.getFiles();
+				final StringBuilder text = new StringBuilder();
+				for (final File file : files)
+				{
+					text.append(file.getName() + "\n");
+				}
+				fileList.setText(text.toString());
+			}
+		});
+			
+		getGrid().setWidget(0, 0, new Label("Select language for these images"));
+		getGrid().setWidget(0, 1, language);
+		
+		getGrid().setWidget(1, 0, new Label("Select the images to be uploaded"));
+		getGrid().setWidget(1, 1, upload);
+		
+		getGrid().setWidget(2, 0, new Label("This is a list of the images you have selected"));
+		getGrid().setWidget(2, 1, fileList);
+	}
+	
+	public void setEnabled(final boolean enabled)
+	{
+		this.getFileList().setEnabled(enabled);
+		this.getLanguage().setEnabled(enabled);
+		this.getUpload().setEnabled(enabled);
+	}
+}
